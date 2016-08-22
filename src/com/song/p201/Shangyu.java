63ef2c98730c7ce0bcbe21b06e27cbc879f6dc38
yoeman <yoeman@MacBook-Pro.local>
2016-08-22 14:08:22 -0700

    add NEW class Shangyu

---
+++ src/com/song/p201/Shangyu.java
@@ -0,0 +1,9 @@
+package com.song.p201;
+
+public class Shangyu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my name is etof22");
+	}
+}
