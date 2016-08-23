88194177d78060860215dfe1335f499d1a5678f9
yoeman <yoeman@MacBook-Pro.local>
2016-08-22 21:20:59 -0700

    etof25

--- src/com/song/p201/Shangyu.java
+++ src/com/song/p201/Shangyu.java
@@ -4,6 +4,6 @@ public class Shangyu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("my name is etof24");
+		System.out.println("my name is etof25");
 	}
 }
