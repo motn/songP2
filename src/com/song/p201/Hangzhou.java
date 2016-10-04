3672a4e32fd2ca1fda9ab86c4f462fe79f315216
Lingyan Song <lucysong818@gmail.com>
2016-10-03 20:25:37 -0700

    add

--- src/com/song/p201/Hangzhou.java
+++ src/com/song/p201/Hangzhou.java
@@ -10,6 +10,31 @@ public class Hangzhou {
 		System.out.println("welcome to hangzhou 3");
 		System.out.println("my name is etof");
 		System.out.println("my name is etof4");
+		
+		String str = null;
+		System.out.println(numOfChar(str));
+		StringBuilder sb = new StringBuilder();
+		System.out.println(sb.toString().isEmpty());
+		System.out.println(new String().length());
+		System.out.println(new String() == null);
+		System.out.println(new String().equals(""));
+		System.out.println(str.length());
 	}
-
+	
+	public static int numOfChar(String str) {
+		int res = 0;
+//		try {
+//			int len = str.length();
+//			for(int i = 0; i < len; i++)
+//				res++;
+//		} catch(NullPointerException e) {
+//			e.printStackTrace();
+//		}
+		if(str == null) {
+			throw new NullPointerException();
+		}
+		for(int i = 0; i < str.length(); i++)
+			res++;
+		return res;
+	}
 }
